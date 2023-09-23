a904553c25ee3794ba50c3e5a5294b4f9d9b1c80
akashkhinde18 <76939436+akashkhinde18@users.noreply.github.com>
2023-09-23 12:15:19 +0530

    Add files via upload

---
+++ ControlStatement/ifStatement1.java
@@ -0,0 +1,9 @@
+public class ifStatement1 {
+    public static void main(String[] args) {
+        boolean Male = true;
+        if(Male)
+        {
+            System.out.println("The person is male");
+        }
+    }
+}
