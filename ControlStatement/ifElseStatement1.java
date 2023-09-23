d20d884f813e82ea5510253ab01cb9dfddc35f20
akashkhinde18 <76939436+akashkhinde18@users.noreply.github.com>
2023-09-23 12:25:26 +0530

    Create ifElseStatement1.java

---
+++ ControlStatement/ifElseStatement1.java
@@ -0,0 +1,9 @@
+public class ifElseStatement1{
+  public static void main(String[] args){
+    boolean Male = false;
+    if(Male){
+    System.out.println("The person is Male");
+    }else
+      System.out.println("The person is Female");
+  }
+}
