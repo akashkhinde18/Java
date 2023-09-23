0ffd23761cd64371f4335c21d55f5d6c3e1f3a79
akashkhinde18 <76939436+akashkhinde18@users.noreply.github.com>
2023-09-23 12:21:06 +0530

    Add files via upload

---
+++ ControlStatement/ifElseStatement.java
@@ -0,0 +1,12 @@
+public class ifElseStatement {
+    public static void main(String[] args) {
+        int x=10;
+        int y=10;
+        if(x+y>30)
+        {
+            System.out.println("x+y is greater than 30 ");
+        }
+        else
+        System.out.println("x+y is less than 30");
+    }
+}
